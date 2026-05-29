4d30bdb609e60787fbb18424c05483c019ef82bd
Carlos Verdugo <carlos.versa99@gmail.com>
2026-05-29 12:03:08 -0700

    Adding courses-list component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoursesList } from './courses-list/courses-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CoursesList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/courses-list/courses-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-list',
+  imports: [],
+  templateUrl: './courses-list.html',
+  styleUrl: './courses-list.css',
+})
+export class CoursesList {
+
+}
